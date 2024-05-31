0f25ae356a9c9d45a121ffa9c43307c8bbad8124
Eric Ogbekene <ericogbekene@icloud.com>
2024-05-31 21:55:23 +0100

    trying to fix 2 then statements

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).catch((error) => new Error(error)).finally(() => { console.log('Got a response from the API'); });
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => {{}}).finally(() => { console.log('Got a response from the API'); });
 }
