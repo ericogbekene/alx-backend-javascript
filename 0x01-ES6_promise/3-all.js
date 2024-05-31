c9060ff737e60cd3edd44d56ed496e9dad23c214
Eric Ogbekene <ericogbekene@icloud.com>
2024-05-31 21:12:51 +0100

    task to resolve all promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import {uploadPhoto, createUser} from './utils'
+export default function handleProfileSignup(){
+        Promise.all([uploadPhoto(), createUser()]).then((results) => {
+            // Destructure the results array to get the photo and user
+            const [photo, user] = results;
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        }).catch(() => {
+            // Handle any errors by logging the specified message
+            console.error('Signup system offline');
+            });
+    
+}
