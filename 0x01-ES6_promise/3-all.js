a031809a0edee01ec64f4b1174bf5b551dfa041d
Eric Ogbekene <ericogbekene@icloud.com>
2024-05-31 21:41:06 +0100

    fixing to return a promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((results) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((results) => {
     // Destructure the results array to get the photo and user
     const [photo, user] = results;
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
