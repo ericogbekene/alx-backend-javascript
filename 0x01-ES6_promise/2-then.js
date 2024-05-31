602debd1bcd571d80b6c2ebba3c66637c3ef0321
Eric Ogbekene <ericogbekene@icloud.com>
2024-05-31 08:02:49 +0100

    using this to resolve promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,4 @@
+export default function handleResponseFromAPI(promise){
+    return promise.then(() => { console.log('Got a response from the API')}).then(() => {return {status:200, body: 'Success'}})
+    .catch(error => { return new Error(error)})
+}
