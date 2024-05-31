899bb845ecd92b838a7cef0f8b65d2921ffa7ae1
Eric Ogbekene <ericogbekene@icloud.com>
2024-05-31 21:33:20 +0100

    Fixing directory on promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,10 @@
-export default function getResponseFromAPI(){
-	return new Promise((resolve, reject) => {
-		let checker = true;
-		if (checker){
-			resolve('Success');
-		}
-		else{
-			reject('Failed');
-		}
-	}
-	).then((message) => {console.log(message)}).catch((error) => console.error(error) );
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const checker = true;
+    if (checker) {
+      resolve('Success');
+    } else {
+      reject(new Error('Failed'));
+    }
+  }).catch((error) => console.error(error));
 }
