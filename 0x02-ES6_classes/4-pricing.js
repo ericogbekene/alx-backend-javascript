4bf36ce6dbf51a689eb4d4165876a3156406d20b
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-04 12:31:53 +0100

    fixing lint issues for 4-pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,7 +2,7 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number' || !currency) throw new Error('Invalid type of arguments passed');
+    if (typeof amount !== 'number' || (currency instanceof Currency !== true)) throw new Error('Invalid type of arguments passed');
     else {
       this._amount = amount;
       this._currency = currency;
